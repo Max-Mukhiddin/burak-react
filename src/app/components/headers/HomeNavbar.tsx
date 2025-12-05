11d4535c8dcd051b38ddb5c42d02b1ef70409c97
Max-Mukhiddin <@mukhiddinsolijonov101@gmail.com>
2025-12-05 15:30:54 +0900

    feat: practise useState & useEffect hookd

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,15 +1,26 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function HomeNavbar() {
   const authMember = null;
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const [value, setvalue] = useState<boolean>(true);
+
+  useEffect(() => {
+    console.log("componentDidMount"); // Data Fetch
+    setCount(count + 1);
+    return () => {
+      console.log("componentWillUnmount");
+    };
+  }, [value]);
+
+  /** Handlers **/
 
   const buttonHandler = () => {
-    setCount(count + 1);
+    setvalue(!value);
   };
 
   return (
